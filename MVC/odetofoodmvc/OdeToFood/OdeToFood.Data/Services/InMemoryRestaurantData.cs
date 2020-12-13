8e5523ccc57adb6e40117795d7f3433c8aab71c6
robertuntal <robertuntal@gmail.com>
2020-12-12 17:54:11 -0800

    Initial Commit

---
+++ MVC/odetofoodmvc/OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants; 
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.French},
+                new Restaurant {Id = 1, Name = "Ruchi's", Cuisine = CuisineType.Indian},
+                new Restaurant {Id = 1, Name = "Olive Garden", Cuisine = CuisineType.Italian}
+
+            }; 
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            
+        }
+    }
+}
